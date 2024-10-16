eb93bfa9a3f0b95cbf2c4b16196b27aad54a4cbd
TableTopRug <flame03fire@gmail.com>
2024-10-16 18:00:49 -0500

    make attempt to get tile source stuff (it isnt working)

--- Scripts/Test/SplatterTile.cs
+++ Scripts/Test/SplatterTile.cs
@@ -11,15 +11,34 @@ public partial class SplatterTile : TileMapLayer
 		int choice;
 
 		int numTileSources = ts.GetSourceCount();
-		// WHY THE ACTUAL FUCK IS THIS NOT 0-INDEXED???
-		choice = (int)rand.NextInt64(numTileSources) + 1;
+		// WHY THE ACTUAL FUCK IS THIS NOT (consistently) 0-INDEXED???
+		// IT WAS NON-ZERO INDEXED IN THE OTHER FILE SO WHAT THE FUCK?
+		choice = (int)rand.NextInt64(numTileSources);
 		TileSetSource tss = ts.GetSource(choice);
 		GD.Print($"{numTileSources} sources, chose {choice}: {ts.HasSource(choice)}");
 		
 		int numTilesInSource = tss.GetTilesCount();
+		// I gotta fight the GoDot devs
 		choice = (int)rand.NextInt64(numTilesInSource);
 		Vector2I tileAtlasCoords = tss.GetTileId(choice);
-		GD.Print($"{numTilesInSource} tiles, chose {choice}");
+		GD.Print($"{numTilesInSource} tiles, chose {choice}: {tss.HasTile(tileAtlasCoords)}");
+
+		TileSetAtlasSource tsas = (TileSetAtlasSource)tss;
+		Image img = tsas.Texture.GetImage();
+		GD.Print($"Texture: {img}");
+		int imgH = img.GetHeight();
+		int imgW = img.GetWidth();
+		bool[,] alphaMap = new bool[imgH, imgW];
+		GD.Print($"Texture: {img != null}");
+
+		for (int y = 0; y < imgH; y++) {
+			for (int x = 0; x < imgW; x++) {
+				Color pix = img.GetPixel(x, y);
+				alphaMap[y, x] = pix.A > 0;
+			}
+		}
+
+		GD.Print(alphaMap.ToString());
 
 		return new Vector2I[]{new Vector2I(0, 0)};
 	}

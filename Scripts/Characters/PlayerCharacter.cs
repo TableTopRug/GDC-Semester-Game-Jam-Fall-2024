4132e6619debed6eb2d62a1645c60052e0b8b181
TableTopRug <flame03fire@gmail.com>
2024-11-06 15:47:54 -0600

    player movement works fine

--- Scripts/Characters/PlayerCharacter.cs
+++ Scripts/Characters/PlayerCharacter.cs
@@ -1,12 +1,12 @@
 using Godot;
 using System;
 
-public partial class PlayerCharacter : CharacterBody2D
+public partial class PlayerCharacter : Character
 {
 	// public const float Speed = 300.0f;
 	// public const float JumpVelocity = -400.0f;
 
-	public override void _PhysicsProcess(double delta)
+	/* public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
@@ -16,15 +16,13 @@ public partial class PlayerCharacter : CharacterBody2D
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
-	}
+	} */
 }

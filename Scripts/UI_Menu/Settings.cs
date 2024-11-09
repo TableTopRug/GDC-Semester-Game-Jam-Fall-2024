66890d17da65c685b92ab37e211de7900aa4ddd4
TableTopRug <flame03fire@gmail.com>
2024-11-08 19:01:02 -0600

    start on menumusic

--- Scripts/UI_Menu/Settings.cs
+++ Scripts/UI_Menu/Settings.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class Settings : PanelContainer
 {
+	[ExportGroup("Menu Music")]
+	[Export]
+	public float menuMusicVol = 0;
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,7 +18,8 @@ public partial class Settings : PanelContainer
 	{
 	}
 
-	public void OnSliderUpdate() {
-		
+	public void OnSliderValueChanged(float val) {
+		menuMusicVol = val;
+
 	}
 }

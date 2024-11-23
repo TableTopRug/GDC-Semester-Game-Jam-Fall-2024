46c5ca1bdb3b8588ea05ad3b0544b1f938981a44
TableTopRug <flame03fire@gmail.com>
2024-11-22 21:28:10 -0600

    fix the map a bit

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -16,47 +16,7 @@ public abstract partial class Character : CharacterBody2D
     }
 
     public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		// if (!IsOnFloor())
-		// {
-		// 	velocity += GetGravity() * (float)delta;
-		// }
-
-		// Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// {
-		// 	velocity.Y = JumpVelocity;
-		// }
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Vector2.Zero;
-		// direction = Input.GetVector("player_move_left", "player_move_right", "player_move_up", "player_move_down");
-		if (Input.IsActionPressed("player_move_left")) {
-			direction = Vector2I.Left;
-		} else if (Input.IsActionPressed("player_move_right")) {
-			direction = Vector2I.Right;
-		} else if (Input.IsActionPressed("player_move_up")) {
-			direction = Vector2I.Up;
-		} else if (Input.IsActionPressed("player_move_down")) {
-			direction = Vector2I.Down;
-		}
-
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
-		}
-		else
-		{
-			velocity.X = (float)Mathf.MoveToward(Velocity.X, 0, Speed / delta);
-			velocity.Y = (float)Mathf.MoveToward(Velocity.Y, 0, Speed / delta);
-		}
-
-		Velocity = velocity;
+    {
 		MoveAndSlide();
-	}
+    }
 }
--- Scripts/Characters/PlayerCharacter.cs
+++ Scripts/Characters/PlayerCharacter.cs
@@ -3,26 +3,41 @@ using System;
 
 public partial class PlayerCharacter : Character
 {
-	// public const float Speed = 300.0f;
-	// public const float JumpVelocity = -400.0f;
-
-	/* public override void _PhysicsProcess(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		Vector2 direction = Vector2.Zero;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("player_move_left", "player_move_right", "player_move_up", "player_move_down");
+		
+		if (Input.IsActionPressed("player_move_left")) {
+			direction += Vector2I.Left;
+		}
+		if (Input.IsActionPressed("player_move_right")) {
+			direction += Vector2I.Right;
+		}
+		if (Input.IsActionPressed("player_move_up")) {
+			direction += Vector2I.Up;
+		}
+		if (Input.IsActionPressed("player_move_down")) {
+			direction += Vector2I.Down;
+		}
+
+		GD.Print($"{direction}");
 		if (direction != Vector2.Zero)
 		{
+			direction = direction.X == direction.Y ? direction * .7f : direction;
+			
 			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = (float)Mathf.MoveToward(Velocity.X, 0, Speed / delta);
+			velocity.Y = (float)Mathf.MoveToward(Velocity.Y, 0, Speed / delta);
 		}
 
 		Velocity = velocity;
-		MoveAndSlide();
-	} */
+
+		base._PhysicsProcess(delta);
+	}
 }
